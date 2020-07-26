6c018eedb562043e8c70c127dc0636eff87bac71
林锦骏 <673115816@qq.com>
2020-07-26 23:57:50 +0800

    homework16

---
+++ week16/saturday/animate.js
@@ -0,0 +1,177 @@
+export class TimeLine {
+    constructor() {
+        this.animations = []
+        this.requestID = null
+        this.state = 'pending'
+    }
+    tick() {
+        let t = Date.now() - this.startTime
+        //this.animations = this.animations.filter(animation => animation.finish !== true)
+        let allFinish = this.animations.some(animation => animation.finish !== true)
+        for (let animation of this.animations) {
+            if (animation.finish) {
+                continue
+            }
+            let { object, property, template, start, end, duration, delay = 0, timingFunction } = animation
+            if (t > duration + delay) {
+                animation.finish = true
+                t = duration + delay
+            }
+            let process = timingFunction(t / duration)
+            let value = start + (end - start) * process
+
+            object[property] = template(value)
+        }
+
+        this.requestID = requestAnimationFrame(() => this.tick())
+        // if (allFinish) {
+        // } else {
+        //     this.state = 'finish'
+        // }
+    }
+    start() {
+        this.state = 'running'
+        this.startTime = Date.now()
+        this.tick()
+    }
+
+    pause() {
+        if (this.requestID !== null && this.state === 'running') {
+            this.state = 'paused'
+            this.endTime = Date.now()
+            cancelAnimationFrame(this.requestID)
+        }
+    }
+
+    resume() {
+        if (this.state === 'paused') {
+            this.state = 'running'
+            this.startTime += Date.now() - this.endTime
+            this.tick()
+        }
+    }
+
+    clear() {
+        if (this.state !== 'finish') {
+            this.state = 'finish'
+            cancelAnimationFrame(this.requestID)
+            for (const animation of this.animations) {
+                let { object, property, template, start, end, duration, delay = 0, timingFunction } = animation
+                object[property] = template(start + end)
+                animation.finish = true
+            }
+        }
+    }
+
+    reset() {
+        this.animations.map(animation => animation.finish = false)
+        this.start()
+    }
+
+    add(animation) {
+        this.animations.push(animation)
+    }
+}
+export class ColorTimeLine {
+    constructor() {
+        this.animations = []
+        this.requestID = null
+        this.state = 'pending'
+    }
+    tick() {
+        let t = Date.now() - this.startTime
+        //this.animations = this.animations.filter(animation => animation.finish !== true)
+        let allFinish = this.animations.some(animation => animation.finish !== true)
+        for (let animation of this.animations) {
+            if (animation.finish) {
+                continue
+            }
+            let { object, property, template, start, end, duration, delay = 0, timingFunction } = animation
+            if (t > duration + delay) {
+                animation.finish = true
+                t = duration + delay
+            }
+            let [r1, g1, b1] = start.match(/(\d{1,3})/g)
+            let [r2, g2, b2] = end.match(/(\d{1,3})/g)
+            let process = timingFunction(t / duration)
+            let value1 = r1 * 1 + (r2 * 1 - r1 * 1) * process
+            let value2 = g1 * 1 + (g2 * 1 - g1 * 1) * process
+            let value3 = b1 * 1 + (b2 * 1 - b1 * 1) * process
+
+            object[property] = template(`${value1},${value2},${value3}`)
+        }
+
+        if (allFinish) {
+            this.requestID = requestAnimationFrame(() => this.tick())
+        } else {
+            this.state = 'finish'
+        }
+    }
+    start() {
+        this.state = 'running'
+        this.startTime = Date.now()
+        this.tick()
+    }
+
+    pause() {
+        if (this.requestID !== null && this.state === 'running') {
+            this.state = 'paused'
+            this.endTime = Date.now()
+            cancelAnimationFrame(this.requestID)
+        }
+    }
+
+    resume() {
+        if (this.state === 'paused') {
+            this.state = 'running'
+            this.startTime += Date.now() - this.endTime
+            this.tick()
+        }
+    }
+
+    clear() {
+        if (this.state !== 'finish') {
+            this.state = 'finish'
+            cancelAnimationFrame(this.requestID)
+            for (const animation of this.animations) {
+                let { object, property, template, start, end, duration, delay = 0, timingFunction } = animation
+                object[property] = template(start + end)
+                animation.finish = true
+            }
+        }
+    }
+
+    reset() {
+        this.animations.map(animation => animation.finish = false)
+        this.start()
+    }
+
+    add(animation) {
+        this.animations.push(animation)
+    }
+}
+
+export class Animation {
+    constructor(object, property, template, start, end, duration, delay, timingFunction) {
+        this.object = object
+        this.property = property
+        this.template = template
+        this.start = start
+        this.end = end
+        this.duration = duration
+        this.delay = delay
+        this.timingFunction = timingFunction
+    }
+}
+export class ColorAnimation {
+    constructor(object, property, template, start, end, duration, delay, timingFunction) {
+        this.object = object
+        this.property = property
+        this.template = template
+        this.start = start
+        this.end = end
+        this.duration = duration
+        this.delay = delay
+        this.timingFunction = timingFunction
+    }
+}
---
+++ week16/saturday/main.js
@@ -0,0 +1,126 @@
+const createElement = require('./createElement.js')
+
+import { ease } from './cubicBezier.js'
+import { TimeLine, Animation } from './animate.js'
+class Carousel {
+    constructor() {
+        this.children = []
+        this.attrs = new Map
+        this.props = new Map
+    }
+
+    setAttribute(name, attr) {
+        this[name] = attr
+        // this.root.setAttribute(name, attr)
+    }
+
+    appendChildren(child) {
+        this.children.push(child)
+    }
+
+    mount(parent) {
+        // parent.append(this.root)
+        // for (let child of this.children) {
+        //     child.mount(this.root)
+        // }
+        this.render().mount(parent)
+    }
+
+    render() {
+        let child = this.data.map(url => {
+            let ele = <img src={url} />
+            ele.addEventListener('dragstart', (e) => e.preventDefault())
+            return ele
+        })
+        let root = (
+            <div class="carousel">
+                {
+                    child
+                }
+            </div>
+        )
+        let position = 0
+        let timeLine = new TimeLine
+        timeLine.start()
+        let nexPic = () => {
+            let nextPostion = (position + 1) % child.length
+            let curr = child[position]
+            let next = child[nextPostion]
+
+            let currAnimate = new Animation(curr.style, 'transform', v => `translateX(${v}%)`, - 100 * position, -100 - 100 * position, 400, 0, ease)
+            let nextAnimate = new Animation(next.style, 'transform', v => `translateX(${v}%)`, 100 - 100 * nextPostion, -100 * nextPostion, 400, 0, ease)
+            timeLine.add(currAnimate)
+            timeLine.add(nextAnimate)
+            position = nextPostion
+            // curr.style.transition = 'ease 0s'
+            // next.style.transition = 'ease 0s'
+            // curr.style.transform = `translateX(${- 100 * position}%)`
+            // next.style.transform = `translateX(${100 - 100 * nextPostion}%)`
+            // window.innerWidth
+            // setTimeout(() => {
+                // curr.style.transition = 'ease 0.4s'
+                // next.style.transition = 'ease 0.4s'
+                // curr.style.transform = `translateX(${-100 - 100 * position}%)`
+                // next.style.transform = `translateX(${-100 * nextPostion}%)`
+            // }, 16);
+            setTimeout(nexPic, 3000);
+        }
+        setTimeout(nexPic, 3000)
+
+        root.addEventListener('mousedown', (e) => {
+            let startX = e.clientX, startY = e.clientY
+            let length = child.length
+            let lastPosition = (position - 1 + length) % length
+            let nextPosition = (position + 1) % length
+            let curr = child[position]
+            let width = curr.clientWidth
+            let last = child[lastPosition]
+            let next = child[nextPosition]
+
+            curr.style.transition = 'ease 0s'
+            last.style.transition = 'ease 0s'
+            next.style.transition = 'ease 0s'
+
+            curr.style.transform = `translateX(${- width * position}px)`
+            last.style.transform = `translateX(${-width - width * lastPosition}px)`
+            next.style.transform = `translateX(${width - width * nextPosition}px)`
+
+            let move = e => {
+                curr.style.transform = `translateX(${- width * position + e.clientX - startX}px)`
+                last.style.transform = `translateX(${-width - width * lastPosition + e.clientX - startX}px)`
+                next.style.transform = `translateX(${width - width * nextPosition + e.clientX - startX}px)`
+            }
+            let up = e => {
+                let offset = 0
+                if (e.clientX - startX > width / 4) {
+                    offset = -1
+                } else if (e.clientX - startX < -width / 4) {
+                    offset = 1
+                }
+                curr.style.transition = 'ease 0.4s'
+                last.style.transition = 'ease 0.4s'
+                next.style.transition = 'ease 0.4s'
+
+                curr.style.transform = `translateX(${- width * position - offset * width}px)`
+                last.style.transform = `translateX(${-width - width * lastPosition - offset * width}px)`
+                next.style.transform = `translateX(${width - width * nextPosition - offset * width}px)`
+                position = (position + offset + length) % length
+                document.removeEventListener('mousemove', move)
+                document.removeEventListener('mouseup', up)
+            }
+            document.addEventListener('mousemove', move)
+            document.addEventListener('mouseup', up)
+        })
+        return root
+    }
+}
+
+/** @jsx createElement  */
+let carousel = <Carousel data={[
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+]} />
+
+carousel.mount(document.body)
