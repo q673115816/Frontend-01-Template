f89e0a9c73ae9869a2c9f3af81b4e327b211f6d7
林锦骏 <673115816@qq.com>
2020-07-29 23:28:17 +0800

    homework16

--- week16/saturday/animate.js
+++ week16/saturday/animate.js
@@ -1,54 +1,69 @@
 export class TimeLine {
     constructor() {
-        this.animations = []
+        this.animations = new Set
+        this.finishAnimations = new Set
+        this.addTime = new Map
         this.requestID = null
-        this.state = 'pending'
+        this.state = 'inited'
     }
     tick() {
-        // let t = Date.now() - this.startTime
-        this.animations = this.animations.filter(animation => animation.finish !== true)
-        let allFinish = this.animations.some(animation => animation.finish !== true)
+        let t = Date.now() - this.startTime
+        // this.animations = this.animations.filter(animation => animation.finish !== true)
+        // let allFinish = this.animations.some(animation => animation.finish !== true)
+
         for (let animation of this.animations) {
             if (animation.finish) {
                 continue
             }
             let t = Date.now() - animation.createTime
             let { object, property, template, start, end, duration, delay = 0, timingFunction } = animation
+            let addTime = this.addTime.get(animation)
             if (t > duration + delay) {
-                animation.finish = true
+                // animation.finish = true
                 t = duration + delay
+                this.animations.delete(animation)
             }
             let process = timingFunction(t / duration)
             let value = start + (end - start) * process
             object[property] = template(value)
         }
-
-        this.requestID = requestAnimationFrame(() => this.tick())
+        if (this.animations.size)
+            this.requestID = requestAnimationFrame(() => this.tick())
+        else
+            this.requestID = null
         // if (allFinish) {
         // } else {
         //     this.state = 'finish'
         // }
     }
     start() {
-        this.state = 'running'
+        if (this.state !== 'inited')
+            return
+
+        this.state = 'playing'
         this.startTime = Date.now()
         this.tick()
     }
 
     pause() {
-        if (this.requestID !== null && this.state === 'running') {
-            this.state = 'paused'
-            this.endTime = Date.now()
+        if (this.state !== 'playing')
+            return
+        this.state = 'paused'
+        this.endTime = Date.now()
+
+        if (this.requestID !== null) {
             cancelAnimationFrame(this.requestID)
+            this.requestID = null
         }
     }
 
     resume() {
-        if (this.state === 'paused') {
-            this.state = 'running'
-            this.startTime += Date.now() - this.endTime
-            this.tick()
+        if (this.state !== 'paused') {
+            return
         }
+        this.state = 'playing'
+        this.startTime += Date.now() - this.endTime
+        this.tick()
     }
 
     clear() {
@@ -64,12 +79,37 @@ export class TimeLine {
     }
 
     reset() {
-        this.animations.map(animation => animation.finish = false)
-        this.start()
+        if (this.start === 'playing')
+            this.pause()
+        this.animations = new Set
+        this.addTime = new Map
+        this.requestID = null
+        this.state = 'playing'
+        this.startTime = Date.now()
+        this.tick()
     }
 
-    add(animation) {
-        this.animations.push(animation)
+    restart() {
+        if (this.start === 'playing')
+            this.pause()
+        this.animations = new Set
+        this.addTime = new Map
+        this.requestID = null
+        this.state = 'playing'
+        this.startTime = Date.now()
+        this.tick()
+    }
+
+    add(animation, addTime) {
+        this.animations.add(animation)
+        // animation.finish = false
+        if (this.state === 'playing' && this.requestID === null)
+            this.tick()
+        if (this.state === 'playing')
+            this.addTime.set(animation, this.addTime.get(animation) !== void 0 ? addTime : Date.now() - this.startTime)
+        else
+            this.addTime.set(animation, this.addTime.get(animation) !== void 0 ? addTime : 0)
+
     }
 }
 export class ColorTimeLine {
